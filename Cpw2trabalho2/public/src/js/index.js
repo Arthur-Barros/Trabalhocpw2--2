7c7297427689a4eaeee4ea447b693a78d69af8f9
Arthur Barros <barrosarthur773@gmail.com>
2019-12-02 19:39:58 -0300

    Alteracoes JS

--- Cpw2trabalho2/public/src/js/index.js
+++ Cpw2trabalho2/public/src/js/index.js
@@ -1,6 +1,6 @@
 
 var itens = [{ descricao: 'Abacaxi', unidade: 'Un' }, { descricao: 'Morango', unidade: 'kg' },
-{ descricao: 'Laranja', undidade: 'Un' }, { descricao: 'Limão', unidade: 'un' }]
+{ descricao: 'Laranja', unidade: 'Un' }, { descricao: 'Limão', unidade: 'un' }]
 
 
 
@@ -20,19 +20,48 @@ function adicionar() {
         option.value = i;
         lista.appendChild(option);
 
-        
+        let resposta = document.getElementById('resposta');
 
-
+        let itemSelecionado = i;
+        itemSelecionado = un;
+        resposta.innerText = itemSelecionado;
     }
+}
+
+
+function buscarUnidade () {
+
+}
 
 
 
+function  carregarUnidadeDeMedida(event) {
+    // Inibe a recarga da página
+    event.preventDefault();
 
+    // Recupera os valores do formulário
+    let itemSelecionado = i;
 
+    console.log(itemSelecionado);
+    //console.log(itemSelecionado);
 
+    let divUnidadeDeMedida = document.getElementById('unidadeDeMedida');
+    let unidadeDeMedida = buscarUnidade(itemSelecionado);
+    divUnidadeDeMedida.innerText = unidadeDeMedida
+
+    
+
+    if (unidadeDeMedida == 'kg') {
+        let inputNumber = document.getElementById('quantidade');
+        inputNumber.setAttribute('step', '0.01');
+    } else {
+        document.getElementById('quantidade').value = '';
+        let inputNumber = document.getElementById('quantidade');
+        inputNumber.removeAttribute('step');
+    }
 }
 
 
 adicionar();
 
-
+buscarUnidade();
