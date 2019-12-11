3636e5c7fa2e729c723a26a933fc79ed3a6154df
Arthur Barros <barrosarthur773@gmail.com>
2019-12-11 13:31:18 -0400

    versão 1.0

--- Cpw2trabalho2/public/src/js/index.js
+++ Cpw2trabalho2/public/src/js/index.js
@@ -1,67 +1,261 @@
 
-var itens = [{ descricao: 'Abacaxi', unidade: 'Un' }, { descricao: 'Morango', unidade: 'kg' },
-{ descricao: 'Laranja', unidade: 'Un' }, { descricao: 'Limão', unidade: 'un' }]
+var itens = [{ descricao: 'Abacaxi', unidade: 'UN' }, { descricao: 'Absorvente', unidade: 'PCT' },
+{ descricao: 'Achocolatado em pó', unidade: 'KG' }, { descricao: 'Açúcar', unidade: 'KG' },
+{ descricao: 'Água Sanitária', unidade: 'LITRO' }, { descricao: 'Alcaparras', unidade: 'UN' },
+{ descricao: 'Álcool em gel', unidade: 'UN' }, { descricao: 'Alface', unidade: 'MAÇO' },
+{ descricao: 'Alho', unidade: 'KG' }, { descricao: 'Arroz', unidade: 'KG' },
+{ descricao: 'Azeite de Olivia', unidade: 'UN' }, { descricao: 'Azeitona', unidade: 'KG' },
+{ descricao: 'Banana', unidade: 'KG' }, { descricao: 'Batata', unidade: 'KG' },
+{ descricao: 'Batata Palha', unidade: 'PCT' }, { descricao: 'Beterraba', unidade: 'KG' },
+{ descricao: 'Bolacha', unidade: 'PCT' }, { descricao: 'Café', unidade: 'KG' },
+{ descricao: 'Calabresa', unidade: 'KG' }, { descricao: 'Carne Bovína', unidade: 'KG' },
+{ descricao: 'Carne Suína', unidade: 'KG' }, { descricao: 'Carvão', unidade: 'kg' },
+{ descricao: 'Cebola', unidade: 'KG' }, { descricao: 'Cenoura', unidade: 'KG' },
+{ descricao: 'Cerveja', unidade: 'UN' }, { descricao: 'Chá', unidade: 'CX' },
+{ descricao: 'Cheiro-Verde', unidade: 'MAÇO' }, { descricao: 'Chocolate', unidade: 'KG' },
+{ descricao: 'Condicionador', unidade: 'UN' }, { descricao: 'Copo descartável', unidade: 'PCT' },
+{ descricao: 'Coxa sobre Coxa', unidade: 'KG' }, { descricao: 'Creme de leite', unidade: 'UN' },
+{ descricao: 'Creme dental', unidade: 'UN' }, { descricao: 'Iogurte', unidade: 'UN' },
+{ descricao: 'Desinfetante', unidade: 'LITRO' }, { descricao: 'Erva Tererá', unidade: 'KG' },
+{ descricao: 'Ervilha em conserva', unidade: 'KG' }, { descricao: 'Escova de dente', unidade: 'UN' },
+{ descricao: 'Extrato de Tomate', unidade: 'KG' }, { descricao: 'Farinha de Mandioca', unidade: 'KG' },
+{ descricao: 'Feijão', unidade: 'KG' }, { descricao: 'Fermento Quimico', unidade: 'UN' },
+{ descricao: 'Fio dental', unidade: 'UN' }, { descricao: 'Fósforo', unidade: 'CX' },
+{ descricao: 'Frango', unidade: 'KG' }, { descricao: 'Fubá', unidade: 'KG' },
+{ descricao: 'Gel de cabelo', unidade: 'UN' }, { descricao: 'Hot Pocket', unidade: 'UN' },
+{ descricao: 'Laranja', unidade: 'KG' }, { descricao: 'Leite', unidade: 'LITRO' },
+{ descricao: 'Leite Condensado', unidade: 'CX' }, { descricao: 'Lenha', unidade: 'KG' },
+{ descricao: 'Limão', unidade: 'KG' }, { descricao: 'Luva de latex', unidade: 'PCT' },
+{ descricao: 'Maçã', unidade: 'KG' }, { descricao: 'Macarrão', unidade: 'KG' },
+{ descricao: 'Macarrão instantaneo', unidade: 'UN' }, { descricao: 'Mamão', unidade: 'KG' },
+{ descricao: 'Manteiga', unidade: 'KG' }, { descricao: 'Margarina', unidade: 'KG' },
+{ descricao: 'Melancia', unidade: 'KG' }, { descricao: 'Melão', unidade: 'KG' },
+{ descricao: 'Milho verde em conserva', unidade: 'KG' }, { descricao: 'Mistura de bolo', unidade: 'PCT' },
+{ descricao: 'Molho de Pimenta', unidade: 'L' }, { descricao: 'Molho de Soja', unidade: 'L' },
+{ descricao: 'Mussarela', unidade: 'KG' }, { descricao: 'Óleo de soja', unidade: 'UN' },
+{ descricao: 'Ovos', unidade: 'DZ' }, { descricao: 'Pá de lixo', unidade: 'UN' },
+{ descricao: 'Palmito', unidade: 'KG' }, { descricao: 'Pano de chão', unidade: 'UN' },
+{ descricao: 'Pão', unidade: 'UN' }, { descricao: 'Papel Aluminio', unidade: 'UN' },
+{ descricao: 'Papel higiênico', unidade: 'PCT' }, { descricao: 'Papel Manteiga', unidade: 'UN' },
+{ descricao: 'Peixe', unidade: 'KG' }, { descricao: 'Pêra', unidade: 'KG' },
+{ descricao: 'Pimenta do Reino', unidade: 'G' }, { descricao: 'Pimentão', unidade: 'KG' },
+{ descricao: 'Milho de Pipoca', unidade: 'KG' }, { descricao: 'Plástico filme', unidade: 'UN' },
+{ descricao: 'Presunto', unidade: 'KG' }, { descricao: 'Rabanete', unidade: 'KG' },
+{ descricao: 'Refrigerente', unidade: 'UN' }, { descricao: 'Rodo', unidade: 'UN' },
+{ descricao: 'Rúcula', unidade: 'MAÇO' }, { descricao: 'Sabão em barra', unidade: 'KG' },
+{ descricao: 'Sabão em pó', unidade: 'KG' }, { descricao: 'Sabonete', unidade: 'UN' },
+{ descricao: 'Saco de lixo', unidade: 'PCT' }, { descricao: 'Sal', unidade: 'KG' },
+{ descricao: 'Shampoo', unidade: 'UN' }, { descricao: 'Steak de Frango', unidade: 'UN' },
+{ descricao: 'Tomate', unidade: 'KG' }, { descricao: 'Farinha de Trigo', unidade: 'KG' },
+{ descricao: 'Uva', unidade: 'KG' }, { descricao: 'Vassoura', unidade: 'UN' },
+{ descricao: 'Vinagre', unidade: 'UN' }, { descricao: 'Vinho', unidade: 'UN' }]
 
+var lista = [];
 
-
+function salvar(itens) {
+    lista.push(itens);
+}
 
 function adicionar() {
 
     for (let i = 0; i < itens.length; i++) {
 
-        let desc = itens[i].descricao;
-        let un = itens[i].unidade;
-
         let lista = document.getElementById('select');
-
 
         let option = document.createElement('option');
         option.text = itens[i].descricao;
         option.value = i;
         lista.appendChild(option);
 
-        let resposta = document.getElementById('resposta');
-
-        let itemSelecionado = i;
-        itemSelecionado = un;
-        resposta.innerText = itemSelecionado;
     }
 }
 
+function incluirItem(event) {
+    
+    event.preventDefault();
+    let select = document.getElementById('select').value;
+    let quantidade = document.getElementById('quantidade').value;
+    let unidadeDeMedida = itens[select].unidade;
+    
+    let Minhalista = "";
 
-function buscarUnidade () {
+    Minhalista.innerText = itens[select].descricao, unidadeDeMedida, quantidade;
+    
+    salvar(Minhalista);
 
+    limparFormulario();
+
+    let lista = recuperarTodos();
+
+    document.getElementById('filtro').value = '';
+
+    renderizarlista(lista);
 }
 
 
-
-function  carregarUnidadeDeMedida(event) {
+function carregarUnidadeDeMedida(event) {
     // Inibe a recarga da página
     event.preventDefault();
-
     // Recupera os valores do formulário
-    let itemSelecionado = i;
+    let itemSelecionado = document.getElementById('select').value;
 
-    console.log(itemSelecionado);
-    //console.log(itemSelecionado);
+    let unidadeDeMedida = document.getElementById('unidadeDeMedida');
+    unidadeDeMedida.innerText = itens[itemSelecionado].unidade;
 
-    let divUnidadeDeMedida = document.getElementById('unidadeDeMedida');
-    let unidadeDeMedida = buscarUnidade(itemSelecionado);
-    divUnidadeDeMedida.innerText = unidadeDeMedida
+    // if (unidadeDeMedida === 'KG') {
+    //     console.log('entrei aqui');
+    //     let inputNumber = document.getElementById('quantidade');
+    //     inputNumber.setAttribute('step', '0.01');
+    // } else {
+    //     document.getElementById('quantidade').value = '';
+    //     let inputNumber = document.getElementById('quantidade');
+    //     inputNumber.removeAttribute('step');
+    // }
 
+
+}
+
+
+function renderizarlista(lista) {
     
+    let areaListagemCompras = document.getElementById('listaDosItens');
+    areaListagemCompras.innerHTML = '';
+    if (lista.length > 0) {
+        
+        let table = document.createElement('table');
+        table.setAttribute('id','tabelaProdutos')
 
-    if (unidadeDeMedida == 'kg') {
-        let inputNumber = document.getElementById('quantidade');
-        inputNumber.setAttribute('step', '0.01');
+        let corpoDaLista = corpoLista(lista);
+       
+        table.appendChild(corpoDaLista);
+
+        areaListagemCompras.appendChild(table);   
+
     } else {
-        document.getElementById('quantidade').value = '';
-        let inputNumber = document.getElementById('quantidade');
-        inputNumber.removeAttribute('step');
+        let nenhumValor = document.createElement('span');
+        nenhumValor.setAttribute("id", "aviso");
+        nenhumValor.innerText = 'Nenhum item selecionado !!';
+        areaListagemCompras.appendChild(nenhumValor);
+
     }
 }
+
+function corpoLista(lista) {
+
+    /**
+        * Cria o corpo da tabela
+        */
+    let corpoTabela = document.createElement('tbody');
+    
+    for (let i = 0; i < lista.length; i++) {
+
+        let linha = document.createElement('tr');
+       
+        let check = document.createElement('td');
+
+        //criar a imagem (innerHTML ou createElement img)
+        check.innerHTML = '<img onclick = trocarImg(' + i + ')"id="trocarImg' + i + '" src="src/img/carrinhodecompra.png"></img>';
+        linha.appendChild(check);
+
+        let valores = document.createElement('td');
+        valores.innerText = lista[i].quantidade + ' ' + lista[i].unidade + ' de ' + lista[i].descricao;
+        linha.appendChild(valores);
+
+        let excluir = document.createElement('td');
+
+        excluir.innerHTML = '<a href="#" onclick="deleteRow(\'' + lista[i].descricao + '\');"><img src="src/img/lixeira.png"></a>';
+        linha.appendChild(excluir);
+
+        //Adiciona a nova linha no corpo da tabela
+        corpoTabela.appendChild(linha);
+    }
+    return corpoTabela;
+
+}
+
+function trocarImg(indice) {
+
+    let img = document.getElementById(`trocarImg${indice}`).src;
+
+    if (img.indexOf('check.png') != -1) {
+        document.getElementById(`trocarImg${indice}`).src = './src/img/carrinhodecompra.png';
+    }else{
+        document.getElementById(`trocarImg${indice}`).src = 'src/img/check.png';
+    }
+
+}
+
+
+function limparFormulario() {
+    [
+        'select',
+        'quantidade'
+    ].forEach(id => document.getElementById(id).value = '');
+
+    document.getElementById('unidadeDeMedida').innerText = '';
+}
+
+
+function removerItem(descricao) {
+    for (let i = 0; i < lista.length; i++) {
+        if (descricao == lista[i].descricao) {
+            lista.splice(i, 1);
+        }
+    }
+}
+
+
+function recuperarTodos() {
+    return lista;
+}
+
+function deleteRow(descricao) {
+    removerItem(descricao);
+    let listaRecuperada = recuperarTodos();
+    renderizarlista(listaRecuperada);
+}
+
+
+// function filtrar(lista) {
+//     let listaFiltrada = lista;
+//     if (lista.length > 0) {
+//         listaFiltrada = lista.filter(function (lista) {
+//             let descricao = lista.itens.descricao.toLowerCase();
+//             let unidade = lista.itens.unidade.toLowerCase();
+
+//             /**
+//              * Se o nome ou o e-mail do contato
+//              * conter o filtro do usuário, retorno
+//              * o contato
+//              */
+
+//             return descricao.includes(filtro) || unidade.includes(filtro)
+//         });
+
+//     }
+
+//     return listaFiltrada;
+// }
+
+
+// function filtrarLista() {
+
+//     let filtro = document.getElementById('filtro').value;
+//     filtro = filtro.toLowerCase();
+//     console.log(filtro);
+
+//     /**
+//      * Filtra as Compras de acordo
+//      * com o texto digitado pelo 
+//      * usuário no campo de filtro
+//      */
+//     filtro.innerHTML = filtrar(lista);
+
+//    return filtro;
+
+// }
 
 
 adicionar();
 
-buscarUnidade();
+
